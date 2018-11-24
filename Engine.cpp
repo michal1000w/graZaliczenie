7ed8716cc05b60b694ba8eae9f9d3d888058701a
michal1000w <michal_wieczorek@hotmail.com>
2018-11-24 17:51:08 +0100

    Drawing player & updated FillEmpty()

--- Engine.cpp
+++ Engine.cpp
@@ -10,14 +10,24 @@ class Engine{
 private:
     EasyConsole eCon;
     char board[sizeY][sizeX];
+    struct player{
+        int X;
+        int Y;
+        char Char;
+    };
+    player Player;
     
 public:
     void Init(){
-        FillEmpty();
+        FillEmpty(); //inicjowanie tablicy zerami
+        Player.X = sizeX/2;
+        Player.Y = sizeY/2;
+        Player.Char = '&';
     }
     
     void MainLoop(){
         DrawFullBoard();
+        DrawPlayer();
         
         while(true){
             char keyPressed = getch();
@@ -30,7 +40,7 @@ private:
     void FillEmpty(){
         for (int y=0; y<sizeY; y++)
             for (int x=0; x<sizeX; x++)
-                board[y][x] = '#';
+                board[y][x] = ' ';
     }
     
     void DrawFullBoard(){
@@ -44,5 +54,23 @@ private:
             printw("|\n");
         }
         for (int i=0; i<sizeX+2; i++) printw("=");
+    }
+    
+    void UpdateBoard(int y, int x){
+        move(y+1,x+1);
+        if (board[y][x] == Player.Char){
+            eCon.Color(2);
+            eCon.BoldText(true);
+            printw("%c",board[y][x]);
+            eCon.Color(1);
+            eCon.BoldText(false);
+        } else {
+            printw("%c",board[y][x]);
+        }
+    }
+    
+    void DrawPlayer(){
+        board[Player.Y][Player.X] = Player.Char;
+        UpdateBoard(Player.Y,Player.X);
     }
 };
